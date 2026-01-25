5049fb205f46b0dcfe23ede5c0af6418e8ebcdf6
RileDev <71150306+RileDev@users.noreply.github.com>
2026-01-25 16:26:30 +0100

    Add UI button component and update project config
    Introduced a reusable Button component with variant support in src/components/ui/button.tsx and supporting utility in src/lib/utils.ts. Updated App.tsx to use the new Button and applied a new background/text color scheme. Enhanced Tailwind CSS configuration in index.css with custom themes and variants. Added several dependencies for UI and routing, and configured path aliases in tsconfig and Vite for improved imports. Added components.json for UI component management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Button} from "./components/ui/button.tsx";
 
 const App = () => {
     return (
-        <h1>IT354 Projekat - Polovnjaci.com</h1>
+        <div className="bg-background text-foreground flex min-h-svh flex-col items-center justify-center">
+            <h1>IT354 Projekat - Polovnjaci.com</h1>
+            <Button className="cursor-pointer">Click me</Button>
+        </div>
     )
 }
 export default App
